0ba220afa2efb74e32bd1a5339a653ea1e6c95a4
Jamie Oaks <joaks1@gmail.com>
2010-09-06 21:32:36 -0500

    completely switched over to object-oriented code

--- partition_dist.py
+++ partition_dist.py
@@ -3,12 +3,11 @@ import sys
 import random
 import itertools
 from copy import copy
-import logging
-from dendropy.utility.messaging import get_logger
-_LOG = get_logger('partition_dist')
 
-_VERBOSE = False
-_DEBUGGING = False
+import logging
+logging.basicConfig(level=logging.WARNING)
+_LOG = logging.getLogger('partition_dist')
+
 _RNG = random.Random()
 
 class Subset(object):
@@ -39,7 +38,7 @@ class Subset(object):
         try:
             self._rate = float(float_rate)
         except ValueError, e:
-            sys.stderr.write("Invalid value specified for rate attribute of Subset object: %s" % str(float_rate))
+            _LOG.error("Invalid value specified for rate attribute of Subset object: %s" % str(float_rate))
             raise
     rate = property(_get_rate, _set_rate)
         
@@ -54,7 +53,9 @@ class Subset(object):
     indices = property(_get_indices)
 
     def get_indices_str(self):
-        s = " ".join([str(x+1) for x in self._site_indices])
+        l = [i for i in self.indices]
+        l.sort()
+        s = " ".join([str(x+1) for x in l])
         return s
         
     def _get_number_of_sites(self):
@@ -86,7 +87,7 @@ class Partition(object):
             try:
                 self._lnL = float(lnL)
             except ValueError, e:
-                sys.stderr.write("Invalid value specified for lnL attribute of Partition object: %s" % str(lnL))
+                _LOG.error("Invalid value specified for lnL attribute of Partition object: %s" % str(lnL))
                 raise
         else:
             self._lnL = lnL
@@ -191,6 +192,14 @@ class Partition(object):
                 assert (site_to_subset_indices[site_index] is None)
                 site_to_subset_indices[site_index] = ss_index
         return site_to_subset_indices
+    
+    def get_rate_indices(self):
+        rate_indices = [None] * self.length
+        for ss_index, subset in enumerate(self.subsets):
+            for site_index in subset.indices:
+                assert (rate_indices[site_index] is None)
+                rate_indices[site_index] = subset.rate
+        return rate_indices
         
     def permuted_copy(self):
         self._number_of_permutations += 1
@@ -241,7 +250,7 @@ class PosteriorOfPartitions(object):
     def distance_matrix(self):
         lower_triangle = []
         for n, partition in enumerate(self.partitions):
-            _LOG.debug("Calc distance matrix row %d\n" % n)
+            _LOG.info("Calc distance matrix row %d\n" % n)
             row = self.partitions[:n+1]
             distances = tuple(partition.distance(j) for j in row)
             _LOG.debug("%s\n" % "\t".join([str(x) for x in distances]))
@@ -289,7 +298,20 @@ class PosteriorOfPartitions(object):
         sys.stdout.write("number of ties: %d\n" % tie)
         sys.stdout.write("probability the posterior of partitions is closer to\ntested partition than by chance:\n")
         sys.stdout.write("%f\n" % prob)
+        sys.stdout.write("#######################################################\n")
         return prob
+    
+    def get_mb_partitions(self):
+        mb_parts = []
+        for part in self.partitions:
+            site_to_subset_indices = part.get_site_to_subset_indices()
+            rate_indices = part.get_rate_indices()
+            assert len(site_to_subset_indices) == len(rate_indices)
+            mb_string = "%s\t%d\t%s\t%s\n" % (part.id, part.number_of_subsets, "\t".join([str(x) for x in site_to_subset_indices]), "\t".join([str(r) for r in rate_indices]))
+            mb_parts.append(mb_string)
+        assert (len(mb_parts) == self.number_of_partitions)
+        return mb_parts
+
     
 def parse_mb_header(h):
     s = h.split()
@@ -307,7 +329,7 @@ def read_mb_partitions(sampled_partitions_file, from_row, to_row=None, read_rate
     where the number of subset numbers corresponds to the number of characters
     in the data matrix.
     """
-    d = []
+    posterior = PosteriorOfPartitions()
     line_iter = iter(sampled_partitions_file)
     header = line_iter.next()
     n_sites = parse_mb_header(header)
@@ -320,99 +342,31 @@ def read_mb_partitions(sampled_partitions_file, from_row, to_row=None, read_rate
         s = x.split()
         name = s.pop(0)
         num_elements = int(s.pop(0))
-        list_of_subsets = []
+        partition = Partition([], id = name)
         assignments = s[:n_sites]
-        if read_rates:
-            rates = s[n_sites:]
-            assert(len(assignments) == len(rates))
-        else:
-            rate = 1.0
-        _LOG.debug("reading sample %d\n" % line_num)
+        assignment_set = set(assignments)
+        for i in xrange(len(assignment_set)):
+            partition.add_subset(Subset())
+        rates = s[n_sites:]
+        assert(len(assignments) == len(rates))
+        _LOG.info("reading sample %d\n" % line_num)
         for index, el in enumerate(assignments):
             iel = int(el)
-            while iel > len(list_of_subsets):
+            partition.subsets[iel - 1].add_index(index)
+            if partition.subsets[iel - 1].rate is None:
                 if read_rates:
-                    rate = float(rates[index])
-                subset_info = (set(), rate)
-                list_of_subsets.append(subset_info)
-            set_of_indices = list_of_subsets[iel - 1][0]
-            set_of_indices.add(index)
-        assert len(list_of_subsets) == num_elements
-        d.append((name, list_of_subsets))
-    return d, n_sites
+                    partition.subsets[iel - 1].rate = float(rates[index])
+                else:
+                    partition.subsets[iel - 1].rate = 1.0
+        assert partition.number_of_subsets == len(assignment_set) == num_elements
+        posterior.add_partition(partition)
+    return posterior
 
-def write_mb_partitions(out, sampled_partions, n_sites):
-    for v in sampled_partions:
-        name, los = v
-        c = [None] * n_sites
-        r = [None] * n_sites
-        for set_index, subset_info in enumerate(los):
-            s, rate = subset_info
-            set_num = str(set_index + 1)
-            rate_str = str(rate)
-            for element in s:
-                assert(c[element] is None) # only trips if los is not a partition
-                c[element] = set_num
-                r[element] = rate_str
-        out.write("%s\t%d\t%s\t%s\t\n" % (name, len(los), "\t".join(c), "\t".join(r)))
-
-
-
-def partition_distance(x, y, n_sites):
-    mat = []
-    lx = len(x)
-    ly = len(y)
-    dim = max(lx, ly)
-    for xel in x:
-        row = [0] * dim
-        xset = xel[0]
-        for i, yel in enumerate(y):
-            yset = yel[0]
-            intersection = xset & yset
-            row[i] = len(intersection)
-        mat.append(row)
-    n_to_add = dim - lx
-    for i in xrange(n_to_add):
-        mat.append([0] * dim)
-    
-    cost_matrix = []
-    for row in mat:
-        cost_row = [sys.maxint - col for col in row]
-        cost_matrix.append(cost_row)
-    
-    from munkres import Munkres
-    indexes = Munkres().compute(cost_matrix)
-    
-    total = 0
-    for row, column in indexes:
-        value = mat[row][column]
-        total += value
-    #    print '(%d, %d) -> %d' % (row, column, value)
-    #print 'total cost: %d' % total
-    return n_sites - total
-
-def permute_partition(partition_desc, n_sites):
-    global _RNG
-    site_to_subset_ind = [None] * n_sites
-    permuted = []
-    for ss_index, subset_rate_pair in enumerate(partition_desc):
-        permuted.append([set(), subset_rate_pair[1]])
-        subset = subset_rate_pair[0]
-        for col_index in subset:
-            assert (site_to_subset_ind[col_index] is None)
-            site_to_subset_ind[col_index] = ss_index
-
-    _RNG.shuffle(site_to_subset_ind)
-    
-    for index, iel in enumerate(site_to_subset_ind):
-        assert (iel is not None)
-        set_of_indices = permuted[iel][0]
-        set_of_indices.add(index)
-    return permuted
 
 if __name__ == '__main__':
     from optparse import OptionParser
-    parser = OptionParser()
+    usage = "Usage: %prog [options] <PARTITIONS_FILEPATH>"
+    parser = OptionParser(usage = usage)
     parser.add_option("-s", "--seed", dest="seed", default=0, 
         type="int",
         help="The random number generator seed")
@@ -431,121 +385,43 @@ if __name__ == '__main__':
     parser.add_option("-m", "--median", dest="median", default=False, 
         action="store_true",
         help="Find the sampled partition that has the smallest distance to all of the others (the sample that is closest to being the median)")
-    parser.add_option("-r", "--random", dest="random", default=False, 
-        action="store_true",
-        help="Compare the dist from a specified partition  (the second first partition in the file that is the second arg) to the sampled partitions against the distance from the same specified partition to a random permutation of sampled partition")
+    parser.add_option("--target", dest="target",
+        action="store",
+        type="string",
+        help="Partitions file. The first partition in this file will be used to calculate the probability that the sampled partitions are closer to this target partition than expected by chance")
     (options, args) = parser.parse_args()
-    if options.verbose:
-        _LOG.setLevel(logging.DEBUG)    
     
-    _VERBOSE = options.verbose
-    _DEBUGGING = options.debugging
+    if options.verbose:
+        _LOG.setLevel(logging.INFO)
+    if options.debugging:
+        _LOG.setLevel(logging.DEBUG)
     if options.seed > 0:
         _RNG.seed(options.seed)
     if not args:
         sys.exit("Expecting a file name for the MrBayes DPP-over rates samples")
+
     sampled_partitions_filename = args[0]
     sampled_partitions_file = open(sampled_partitions_filename, 'rU')
-    sampled_partitions, n_sites = read_mb_partitions(sampled_partitions_file, options.from_index, options.to_index)
-    sampled_partitions_file.close()
-    
-    post = PosteriorOfPartitions()
-    for i, p in enumerate(sampled_partitions):
-        part = Partition([], id=p[0])
-        for j, ss in enumerate(p[1]):
-            subset = Subset(ss[0], rate=ss[1])
-            part.add_subset(subset)
-        post.add_partition(part)
-            
+    sampled_partitions = read_mb_partitions(sampled_partitions_file, options.from_index, options.to_index)
+    sampled_partitions_file.close()            
 
-    if _DEBUGGING and _VERBOSE:
-        write_mb_partitions(sys.stdout, sampled_partitions, n_sites)
-    if _VERBOSE:
-        sys.stderr.write("%d partitions read\n" % len(sampled_partitions))
-    if len(args) > 1:
-        test_partitions_filename = args[1]
+    if options.debugging:
+        mb_parts = sampled_partitions.get_mb_partitions()
+        _LOG.debug("%s\n" % "\n".join(mb_parts))
+    _LOG.info("%d partitions read\n" % sampled_partitions.number_of_partitions)
+
+    if options.target:
+        test_partitions_filename = options.target
         test_partitions_file = open(test_partitions_filename, 'rU')
-        test_partitions, test_n_sites = read_mb_partitions(test_partitions_file, 0, 1, read_rates=False)
+        test_partitions = read_mb_partitions(test_partitions_file, 0, 1, read_rates=False)
         test_partitions_file.close()
-        if test_n_sites != n_sites:
+        if test_partitions.partitions[0].length != sampled_partitions.partitions[0].length:
             sys.exit("The number of sites in the test partition must be identical to the number of sites in the sampled partitions from MrBayes")
-        tp = test_partitions[0][1]
+        tp = test_partitions.partitions[0]
+        sampled_partitions.probability_closer_than_random(tp)
         
-        testp = Partition()
-        for s in tp:
-            ss = Subset(s[0], rate = s[1])
-            testp.add_subset(ss)
-        
-        real_tally = 0
-        rand_tally = 0
-        tie_tally = 0
-        total_tally = 0
-        for i in sampled_partitions:
-            real_dist = partition_distance(tp, i[1], n_sites)
-            if options.random:
-                permuted = permute_partition(i[1], n_sites)
-                rand_dist = partition_distance(tp, permuted, n_sites)
-                if real_dist < rand_dist:
-                    real_tally = real_tally + 1
-                elif real_dist > rand_dist:
-                    rand_tally = rand_tally + 1
-                elif real_dist == rand_dist:
-                    tie_tally = tie_tally + 1
-                total_tally = total_tally + 1
-                print "%d\t%d" % (real_dist, rand_dist)
-            else:
-                print real_dist
-        if options.random:
-            assert(real_tally + rand_tally + tie_tally == total_tally == len(sampled_partitions))
-            prob = float(real_tally)/float(total_tally)
-            print "\nNumber of sampled 'wins':  %d" % real_tally
-            print "Number of permuted 'wins':  %d" % rand_tally
-            print "Number of ties:  %d" % tie_tally
-            print "TOTAL:  %d" % total_tally
-            print "Probability that the DPP sampled partitions are closer to a priori partition than random:"
-            print prob
-        
-        post.probability_closer_than_random(testp)
-        
-    else:
-        l = len(sampled_partitions)
-        lower_triangle = []
-        for n, i in enumerate(sampled_partitions):
-            if _VERBOSE:
-                sys.stderr.write("Calc distance matrix row %d\n" % n)
-            r = sampled_partitions[:n+1]
-            d = tuple(partition_distance(i[1], j[1], n_sites) for j in r)
-            if _VERBOSE:
-                sys.stdout.write("%s\n" % "\t".join([str(x) for x in d]))
-            lower_triangle.append(d)
-        if options.median:
-            if _VERBOSE:
-                print lower_triangle
-            dim = len(lower_triangle)
-            sum_dist = [0]*dim
-            for i in xrange(dim):
-                for j in xrange(i):
-                    row = lower_triangle[i]
-                    element = row[j]
-                    sum_dist[i] += element
-                    sum_dist[j] += element
-            min_dist = min(sum_dist)
-            print "samples that are medianish (sum of partition dist of %d):" % min_dist
-            for n, el in enumerate(sum_dist):
-                if el == min_dist:
-                    print sampled_partitions[n][0]
-                    print "\nCharset definitions for %s:\n" % sampled_partitions[n][0]
-                    for i in xrange(len(sampled_partitions[n][1])):
-                        sys.stdout.write("charset ratepart%d =" % (i+1))
-                        for site in sampled_partitions[n][1][i][0]:
-                            sys.stdout.write(" %d" % (site+1))
-                        sys.stdout.write(";\n")
-                    print "\nRates for partition %s:"% sampled_partitions[n][0]
-                    for i in xrange(len(sampled_partitions[n][1])):
-                        print "ratepart%d = %f" % ((i+1), sampled_partitions[n][1][i][1])
-                    print "\n"
-                    
-            med, md = post.median_partition()
-            sys.stdout.write("\n%s\n%d\n" % (str(med), md))
+    if options.median:
+        med, md = sampled_partitions.median_partition()
+        sys.stdout.write("\n%s\n[total distance = %d]\n" % (str(med), md))
                              
                         
