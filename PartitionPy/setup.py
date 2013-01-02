99ee11d79c677241d1f1502fbb002517e72f07bf
Jamie Oaks <joaks1@gmail.com>
2013-01-01 21:19:49 -0600

    Adding script paths to setup.py.

--- PartitionPy/setup.py
+++ PartitionPy/setup.py
@@ -1,6 +1,11 @@
 from setuptools import setup, find_packages
 import sys, os
 
+SCRIPT_SUBPATHS = [
+    ['partitionpy', 'partition_sum.py'],
+ ]
+SCRIPTS = [os.path.join(*i) for i in SCRIPT_SUBPATHS]
+
 version = '0.1'
 
 setup(name='partitionpy',
@@ -11,10 +16,11 @@ setup(name='partitionpy',
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='phylogenetics DPP evolution phylogeny genetics',
       author='Jamie R. Oaks and Mark T. Holder',
-      author_email='joaks1@gmail.com mand mtholder@ku.edu',
+      author_email='joaks1@gmail.com and mtholder@ku.edu',
       url='',
       license='',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      scripts = SCRIPTS,
       include_package_data=True,
       zip_safe=True,
       install_requires=[
